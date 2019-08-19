f9458dbc2e6359c8ba211886ae51f03680c58183
kingswwong <467271883@qq.com>
2019-08-19 19:42:04 +0800

    Add should_item_quality_be_0_when_given_item_name_Other_and_quality_1_and_sellIn_1 test

--- src/main/test/gildedRose/GildedRoseTest.java
+++ src/main/test/gildedRose/GildedRoseTest.java
@@ -20,8 +20,17 @@ public class GildedRoseTest {
     public void should_item_quality_be_1_when_given_item_name_Sulfuras_Hand_of_Ragnaros_and_quality_1_and_sellIn_1() throws Exception {
         Item items[] = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 1, 1)};
         GildedRose gildedRose = new GildedRose(items);
-
+        gildedRose.updateQuality();
         assertEquals(1,gildedRose.getItems()[0].quality);
+
+    }
+
+    @Test
+    public void should_item_quality_be_0_when_given_item_name_Other_and_quality_1_and_sellIn_1() throws Exception {
+        Item items[] = new Item[]{new Item("Other", 1, 1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(0,gildedRose.getItems()[0].quality);
 
     }
 }
