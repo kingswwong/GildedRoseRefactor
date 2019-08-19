21f0e53bf465bf97d66cca91bc6c47c4c8d4d8a0
kingswwong <467271883@qq.com>
2019-08-19 20:37:28 +0800

    Add StrategyModel for Item

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -1,5 +1,8 @@
 package gildedRose;
 
+import gildedRose.item.ItemStrategyFactory;
+import gildedRose.item.ItemStrategyModel;
+
 public class Item {
 
     public String name;
@@ -8,13 +11,49 @@ public class Item {
 
     public int quality;
 
+    public ItemStrategyModel itemStrategyModel;
+
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.itemStrategyModel = ItemStrategyFactory.createItem(name);
     }
 
-   @Override
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public ItemStrategyModel getItemStrategyModel() {
+        return itemStrategyModel;
+    }
+
+    public void setItemStrategyModel(ItemStrategyModel itemStrategyModel) {
+        this.itemStrategyModel = itemStrategyModel;
+    }
+
+    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
---
+++ src/main/java/gildedRose/item/ItemStrategyModel.java
@@ -0,0 +1,10 @@
+package gildedRose.item;
+
+import gildedRose.Item;
+
+/**
+ * Created by KingsWong on 2019/8/19.
+ */
+public interface ItemStrategyModel {
+    void updateQuality(Item item);
+}
