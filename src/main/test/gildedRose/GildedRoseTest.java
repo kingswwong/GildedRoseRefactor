4022404beff5ccfbe9f375dcad8331b18452a5c5
kingswwong <467271883@qq.com>
2019-08-19 20:02:57 +0800

    Add should_item_quality_be_1_when_given_item_name_Sulfuras_Hand_of_Ragnaros_and_quality_1_and_sellIn_minux_1 test

--- src/main/test/gildedRose/GildedRoseTest.java
+++ src/main/test/gildedRose/GildedRoseTest.java
@@ -60,4 +60,15 @@ public class GildedRoseTest {
         assertEquals(0,gildedRose.getItems()[0].sellIn);
 
     }
+
+    @Test
+    public void should_item_quality_be_1_when_given_item_name_Sulfuras_Hand_of_Ragnaros_and_quality_1_and_sellIn_minux_1() throws Exception {
+        Item items[] = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(1,gildedRose.getItems()[0].quality);
+
+    }
+
+
 }
